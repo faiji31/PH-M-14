47d71e4084efd80c3d459341e5ceadad3dad654c
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-02 21:05:02 +0600

    5th commit

---
+++ coke.js
@@ -0,0 +1,6 @@
+let price= 348;
+if (price>=500){
+    console.log("You get Free Coke");
+}else{
+    console.log("You didnt get any free drinks");
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let price= 500;
+if (price>=500){
+    console.log("You get Free Coke");
+}else{
+    console.log("You didnt get any free drinks");
+}
