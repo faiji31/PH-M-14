32db149ffbe4d5a1eb8b91fe97870d3f93d4e724
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-03 00:03:52 +0600

    10th commit

---
+++ num.js
@@ -0,0 +1,14 @@
+let a = 100;
+let b =100;
+let c = 100;
+if (a>b){
+    console.log("a is bigger");
+}else if (b>c){
+    console.log("b is bigger");
+}else if (c>a){
+    console.log("c is bigger");
+}else if(a>c){
+    console.log("a is bigger");
+}else{
+    console.log("all three are the same number")
+}
